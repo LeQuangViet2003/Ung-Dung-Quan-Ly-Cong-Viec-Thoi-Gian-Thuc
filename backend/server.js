4cb048f6f3fba0a603ef3e733fb76e04f560865b
Le Quang Viet <LeQuangViet2003@users.noreply.github.com>
2026-03-17 02:05:07 +0700

    fix: handle path-to-regexp catch-all compatibility

--- backend/server.js
+++ backend/server.js
@@ -186,7 +186,7 @@ const path = require('path');
 const frontendDistPath = path.join(__dirname, '../frontend/dist');
 app.use(express.static(frontendDistPath));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(frontendDistPath, 'index.html'));
 });
 
